47e26aff876c65570d5e5583e355af572148fe0b
tanveer <mdtanviralamht731@gmail.com>
2022-03-30 16:26:49 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import About from "./components/main/About/About";
 import Coins from "./components/main/Coins/Coins";
@@ -18,6 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
