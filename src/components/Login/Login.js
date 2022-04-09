f77da15cfce253505666731bc50f0124d804c75c
tanveer <mdtanviralamht731@gmail.com>
2022-04-09 14:47:40 +0600

    email and password value sent the firebase

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import {
+  createUserWithEmailAndPassword,
   getAuth,
   GithubAuthProvider,
   GoogleAuthProvider,
@@ -7,11 +8,17 @@ import {
 } from "firebase/auth";
 import React, { useState } from "react";
 import app from "../../firebase.init";
+import GoogleIcon from "./1534129544.png";
+import GithubIcon from "./25231.png";
 const auth = getAuth(app);
 
 //login function handler
 const Login = () => {
+  // all use state
   const [user, setUser] = useState({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // firebase provider
   const providerGoogle = new GoogleAuthProvider();
   const providerGithub = new GithubAuthProvider();
   // sign in with github function
@@ -48,6 +55,28 @@ const Login = () => {
         console.error(error);
       });
   };
+  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  // login form submit function
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  const handleFormSubmit = (event) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        const user = res.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    event.preventDefault();
+  };
+  // email auth function
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  // password auth function
+  const handlePassBlur = (event) => {
+    setPassword(event.target.value);
+  };
   return (
     <div className="flex flex-col  items-center ">
       {user.uid ? (
@@ -68,16 +97,45 @@ const Login = () => {
         </div>
       ) : (
         <>
+          {/* form section  */}
+          <form onSubmit={handleFormSubmit} className="w-1/3 mb-8">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              className="py-4 border-2 rounded my-4 w-full px-2 "
+              onBlur={handleEmailBlur}
+              required
+            />
+            <br />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password"
+              className="py-4 border-2 rounded my-4  w-full px-2"
+              onBlur={handlePassBlur}
+              required
+            />
+            <br />
+            <button className="p-4 text-center border-2 rounded capitalize text-xl text-white bg-cyan-500 hover:bg-cyan-400 duration-150 ease-in w-full mt-4">
+              Login
+            </button>
+          </form>
           <button
-            className="p-4 text-center border-2 rounded capitalize text-xl"
+            className="p-4 text-center border-2 rounded capitalize items-center text-xl mb-2 flex text-gray-600"
             onClick={handleBtnGoogle}
           >
+            <img src={GoogleIcon} alt="google" className="w-8 mr-4" />
             sign in with google
           </button>
+          <br />
           <button
-            className="p-4 text-center border-2 rounded capitalize text-xl"
+            className="p-4 text-center border-2 rounded capitalize items-center text-xl flex text-gray-600"
             onClick={handleGithubSignInBtn}
           >
+            <img src={GithubIcon} alt="google" className="w-8 mr-4" />
             sign in with github
           </button>
         </>
