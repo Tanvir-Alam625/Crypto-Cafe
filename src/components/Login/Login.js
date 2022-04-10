ca3d4c3e8c1157bf674e219bd13eda19596ef8bf
tanveer <mdtanviralamht731@gmail.com>
2022-04-10 16:30:45 +0600

    password Validation added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -18,6 +18,10 @@ const Login = () => {
   const [user, setUser] = useState({});
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // error message state
+  const [errorE, setErrorE] = useState("");
+  const [errorP, setErrorP] = useState("");
+  const [errorPC, setErrorPC] = useState("");
   // firebase provider
   const providerGoogle = new GoogleAuthProvider();
   const providerGithub = new GithubAuthProvider();
@@ -59,6 +63,20 @@ const Login = () => {
   // login form submit function
   // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   const handleFormSubmit = (event) => {
+    event.preventDefault();
+    //email validation
+
+    if (!/(\S+@\S+\.\S+)/.test(email)) {
+      setErrorE(" Please enter the valid email!!");
+      return;
+    }
+    // password validation
+    if (!/(?=.*[!@#$%^&*])/.test(password)) {
+      setErrorP("please password must be one spacial character!");
+    }
+    setErrorE("");
+    setErrorP("");
+    // authentication email
     createUserWithEmailAndPassword(auth, email, password)
       .then((res) => {
         const user = res.user;
@@ -67,15 +85,35 @@ const Login = () => {
       .catch((error) => {
         console.error(error);
       });
-    event.preventDefault();
   };
   // email auth function
+
   const handleEmailBlur = (event) => {
-    setEmail(event.target.value);
+    if (/\S+@\S+\.\S+/.test(event.target.value)) {
+      setErrorE("");
+      setEmail(event.target.value);
+    } else {
+      setErrorE(" Please enter the valid email!!");
+    }
   };
   // password auth function
   const handlePassBlur = (event) => {
-    setPassword(event.target.value);
+    if (/(?=.*[!@#$%^&*])/.test(event.target.value)) {
+      setErrorP("");
+      setPassword(event.target.value);
+    } else {
+      setErrorP("please password must be one spacial character!");
+    }
+  };
+  /// confirm password
+  const handleConfirmPassOnChange = (event) => {
+    const cPassword = event.target.value;
+    if (cPassword !== password) {
+      setErrorPC("Your Password Not Match!");
+    } else {
+      setErrorPC("");
+    }
+    console.log(event.target.value);
   };
   return (
     <div className="flex flex-col  items-center ">
@@ -99,32 +137,47 @@ const Login = () => {
         <>
           {/* form section  */}
           <form onSubmit={handleFormSubmit} className="w-1/3 mb-8">
+            <h2 className="text-4xl font-semibold text-center text-cyan-300">
+              SignUp Now
+            </h2>
             <input
               type="email"
               name="email"
               id="email"
               placeholder="Email"
-              className="py-4 border-2 rounded my-4 w-full px-2 "
+              className="py-2 border-2 rounded my-4 w-full px-2 "
               onBlur={handleEmailBlur}
               required
             />
+            <p className="text-red-500 text-xl">{errorE}</p>
             <br />
             <input
               type="password"
               name="password"
               placeholder="Password"
               id="password"
-              className="py-4 border-2 rounded my-4  w-full px-2"
+              className="py-2 border-2 rounded my-4  w-full px-2"
               onBlur={handlePassBlur}
               required
             />
+            <p className="text-red-500">{errorP}</p>
             <br />
-            <button className="p-4 text-center border-2 rounded capitalize text-xl text-white bg-cyan-500 hover:bg-cyan-400 duration-150 ease-in w-full mt-4">
-              Login
+            <input
+              type="password"
+              name="cpass"
+              id="Cpass"
+              className="py-2 border-2 rounded my-4  w-full px-2"
+              placeholder="Confirm Password"
+              onChange={handleConfirmPassOnChange}
+            />
+            <p className="text-red-500">{errorPC}</p>
+            <br />
+            <button className="p-2 text-center border-2 rounded capitalize text-xl text-white bg-cyan-500 hover:bg-cyan-400 duration-150 ease-in w-full mt-4">
+              SignUp
             </button>
           </form>
           <button
-            className="p-4 text-center border-2 rounded capitalize items-center text-xl mb-2 flex text-gray-600"
+            className="p-2 text-center border-2 rounded capitalize items-center text-xl mb-2 flex text-gray-600"
             onClick={handleBtnGoogle}
           >
             <img src={GoogleIcon} alt="google" className="w-8 mr-4" />
@@ -132,7 +185,7 @@ const Login = () => {
           </button>
           <br />
           <button
-            className="p-4 text-center border-2 rounded capitalize items-center text-xl flex text-gray-600"
+            className="p-2 text-center border-2 rounded capitalize items-center text-xl flex text-gray-600"
             onClick={handleGithubSignInBtn}
           >
             <img src={GithubIcon} alt="google" className="w-8 mr-4" />
